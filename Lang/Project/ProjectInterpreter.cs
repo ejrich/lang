be499acd385d4a01910a5e6333ab8f21f0675807
Evan Richardson <ejrich44@gmail.com>
2020-12-05 23:49:06 -0600

    Beginning to read and parse tokens

--- Lang/Project/ProjectInterpreter.cs
+++ Lang/Project/ProjectInterpreter.cs
@@ -1,7 +1,24 @@
-﻿namespace Lang.Project
+﻿using System.IO;
+using System.Linq;
+
+namespace Lang.Project
 {
-    public class ProjectInterpreter
+    public interface IProjectInterpreter
     {
-        
+        Project LoadProject();
+    }
+
+    public class ProjectInterpreter : IProjectInterpreter
+    {
+        public Project LoadProject()
+        {
+            var project = new Project
+            {
+                Name = "Test",
+                BuildFiles = Directory.GetFiles(Directory.GetCurrentDirectory()).ToList() // TODO make this not directory
+            };
+
+            return project;
+        }
     }
 }
