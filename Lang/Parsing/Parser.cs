5c71ba73b1f3e96f01c1df0aab235ad8ab58bba7
Evan Richardson <ejrich44@gmail.com>
2020-12-07 07:50:41 -0600

    Pulling out the lexer

--- Lang/Parsing/Parser.cs
+++ Lang/Parsing/Parser.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 
 namespace Lang.Parsing
 {
@@ -11,6 +9,10 @@ namespace Lang.Parsing
 
     public class Parser : IParser
     {
+        private readonly ILexer _lexer;
+
+        public Parser(ILexer lexer) => _lexer = lexer;
+
         public ParseResult Parse(List<string> projectFiles)
         {
             var result = new ParseResult();
@@ -25,73 +27,11 @@ namespace Lang.Parsing
 
         private void ParseFile(string file)
         {
-            var fileContents = File.ReadAllText(file);
+            // 1. Load file tokens
+            var tokens = _lexer.LoadFileTokens(file);
 
-            var tokens = GetTokens(fileContents);
-            foreach (var token in tokens)
-            {
-                Console.WriteLine($"{token.Type}, {token.Value}");
-            }
-        }
-
-        private IEnumerable<Token> GetTokens(string fileContents)
-        {
-            Token currentToken = null;
-            foreach (var character in fileContents)
-            {
-                if (IgnoreChar(character))
-                {
-                    if (currentToken != null)
-                        yield return currentToken;
-                    currentToken = null;
-                    continue;
-                }
-
-                // Get token from character, determine to emit value
-                var tokenType = GetTokenType(character);
-
-                if (ContinueToken(currentToken, tokenType))
-                {
-                    currentToken.Value += character;
-                }
-                else
-                {
-                    if (currentToken != null)
-                        yield return currentToken;
-
-                    currentToken = new Token
-                    {
-                        Type = tokenType,
-                        Value = $"{character}"
-                    };
-                }
-            }
-
-            if (currentToken != null) yield return currentToken;
-        }
-
-        private static bool IgnoreChar(char token)
-        {
-            return Char.IsWhiteSpace(token);
-        }
-
-        private static TokenType GetTokenType(char character)
-        {
-            var token = (TokenType) character;
-
-            return Enum.IsDefined(typeof(TokenType), token) ? token : TokenType.Token;
-        }
-
-        private static bool ContinueToken(Token currentToken, TokenType type)
-        {
-            if (currentToken == null) return false;
-
-            return currentToken.Type switch
-            {
-                TokenType.Token => type == TokenType.Token,
-                // TODO More validation eventually
-                _ => false
-            };
+            // 2. Iterate through tokens, tracking function definitions
+            
         }
     }
 }
--- Lang/Program.cs
+++ Lang/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 var serviceCollection = new ServiceCollection();
 serviceCollection.AddTransient<ICompiler, Compiler>();
+serviceCollection.AddTransient<ILexer, Lexer>();
 serviceCollection.AddTransient<IParser, Parser>();
 serviceCollection.AddTransient<IProjectInterpreter, ProjectInterpreter>();
 
