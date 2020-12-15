0ad5b93fcab06664c4becf3f1a61647c6cb2c32b
Evan Richardson <ejrich44@gmail.com>
2020-12-15 11:03:32 -0600

    Hooking up the linker

--- Lang/Backend/Assembly/AssemblyLinker.cs
+++ Lang/Backend/Assembly/AssemblyLinker.cs
@@ -2,7 +2,7 @@
 {
     public class AssemblyLinker : ILinker
     {
-        public void Link(string objectPath, string executableName)
+        public void Link(string objectFile, string projectPath)
         {
             throw new System.NotImplementedException();
         }
