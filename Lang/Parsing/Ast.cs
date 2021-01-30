09202ceee9c878886bc3bb1ec9b99e39b0a0b45a
Evan Richardson <ejrich44@gmail.com>
2021-01-29 21:37:54 -0600

    Working on varargs

--- Lang/Parsing/Ast.cs
+++ Lang/Parsing/Ast.cs
@@ -25,6 +25,7 @@ namespace Lang.Parsing
         public int Column { get; init; }
         public string Name { get; set; }
         public bool Extern { get; set; }
+        public bool Varargs { get; set; }
         public TypeDefinition ReturnType { get; set; }
         public List<Argument> Arguments { get; } = new();
         public List<IAst> Children { get; } = new();
