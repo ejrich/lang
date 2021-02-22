1fe0156d028b652588a13e6439554784699a03f8
Evan Richardson <ejrich44@gmail.com>
2021-02-22 17:01:43 -0600

    Adding build settings object to store values from cli

--- Lang/Backend/Assembly/AssemblyBackend.cs
+++ Lang/Backend/Assembly/AssemblyBackend.cs
@@ -2,7 +2,7 @@ namespace Lang.Backend.Assembly
 {
     public class AssemblyBackend : IBackend
     {
-        public void Build(ProgramGraph programGraph, Project.Project project, bool optimize)
+        public void Build(ProgramGraph programGraph, Project.Project project, BuildSettings buildSettings)
         {
             throw new System.NotImplementedException();
         }
