cea37d8a67e807ef298e2fa798a89d7e8aee2614
Evan Richardson <ejrich44@gmail.com>
2021-02-23 07:43:32 -0600

    Setting the linker in the project file

--- Lang/Backend/Assembly/Assembler.cs
+++ Lang/Backend/Assembly/Assembler.cs
@@ -2,7 +2,7 @@ namespace Lang.Backend.Assembly
 {
     public class Assembler : IBuilder
     {
-        public string BuildFile(string filePath)
+        public string BuildFile(string filePath, BuildSettings buildSettings)
         {
             throw new System.NotImplementedException();
         }
