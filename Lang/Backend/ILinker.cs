b8ea7448967310cd939c521344a73a77505e0702
Evan Richardson <ejrich44@gmail.com>
2021-04-05 13:42:16 -0500

    Fixing up code for merge

--- Lang/Backend/ILinker.cs
+++ Lang/Backend/ILinker.cs
@@ -1,5 +1,3 @@
-using Lang.Project;
-
 namespace Lang.Backend
 {
     public interface ILinker
@@ -8,7 +6,7 @@ namespace Lang.Backend
         /// Links the object file and creates an executable file
         /// </summary>
         /// <param name="objectFile">Path to the object file</param>
-        /// <param name="projectPath">The project file representation</param>
+        /// <param name="project">The project to build</param>
         /// <param name="programGraph">The project to build</param>
         /// <param name="buildSettings">Build settings from the cli args</param>
         void Link(string objectFile, ProjectFile project, ProgramGraph programGraph, BuildSettings buildSettings);
