164f337ce67caa8a9bb851484e3788a258f1d2f9
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:31:05 -0600

    Starting on the program runner
    [git-p4: depot-paths = "//lang/main/": change = 265]

--- Lang/Compiler.cs
+++ Lang/Compiler.cs
@@ -34,6 +34,7 @@ namespace Lang
 
         public void Compile(string[] args)
         {
+            new ProgramRunner().RunProgram();
             // 1. Load cli args into build settings
             var buildSettings = new BuildSettings();
             foreach (var arg in args)
