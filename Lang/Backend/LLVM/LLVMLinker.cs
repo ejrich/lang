bf8363cc547de803538c37754f3750bf6a3286f6
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:30:43 -0600

    Starting to programatically search for libs
    [git-p4: depot-paths = "//lang/main/": change = 200]

--- Lang/Backend/LLVM/LLVMLinker.cs
+++ Lang/Backend/LLVM/LLVMLinker.cs
@@ -16,7 +16,12 @@ namespace Lang.Backend.LLVM
             if (!Directory.Exists(binaryPath))
                 Directory.CreateDirectory(binaryPath);
 
-            // 2. Run the linker
+            // 2. Determine lib directories
+            var libDirectory = DetermineLibDirectory();
+            var linker = DetermineLinker(libDirectory);
+            var gccDirectory = DetermineGCCDirectory();
+
+            // 3. Run the linker
             var executableFile = Path.Combine(projectPath, BinaryDirectory, Path.GetFileNameWithoutExtension(objectFile));
             var dependencyList = string.Join(' ', dependencies.Select(d => $"-l{d}"));
             var buildProcess = new Process
@@ -24,12 +29,36 @@ namespace Lang.Backend.LLVM
                 StartInfo =
                 {
                     FileName = "ld",
-                    Arguments = $"-dynamic-linker /usr/lib/ld-2.33.so -o {executableFile} /usr/lib/crt1.o /usr/lib/crti.o {objectFile} " +
-                                $"{dependencyList} -L/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0 --start-group -lgcc -lgcc_eh -lc --end-group /usr/lib/crtn.o"
+                    Arguments = $"-dynamic-linker {linker} -o {executableFile} {objectFile} {DefaultDependencies(libDirectory)} " +
+                                $"{dependencyList} -L{gccDirectory} --start-group -lgcc -lgcc_eh -lc --end-group"
                 }
             };
             buildProcess.Start();
             buildProcess.WaitForExit();
+        }
+
+        private static string DetermineLibDirectory()
+        {
+            return "/usr/lib";
+        }
+
+        private readonly string[] _cRuntimeObjects = {
+            "crt1.o", "crti.o", "crtn.o"
+        };
+
+        private string DefaultDependencies(string libDirectory)
+        {
+            return string.Join(' ', _cRuntimeObjects.Select(o => Path.Combine(libDirectory, o)));
+        }
+
+        private static string DetermineLinker(string libDirectory)
+        {
+            return Path.Combine(libDirectory, "ld-2.33.so");
+        }
+
+        private static string DetermineGCCDirectory()
+        {
+            return "/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0";
         }
     }
 }
