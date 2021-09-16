6c975dfc79f42ae171af66716513088f393f4821
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:29:44 -0600

    Starting work on the parser
    [git-p4: depot-paths = "//lang/main/": change = 12]

--- Lang/Parsing/Parser.cs
+++ Lang/Parsing/Parser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Lang.Parsing
 {
@@ -31,7 +32,57 @@ namespace Lang.Parsing
             var tokens = _lexer.LoadFileTokens(file);
 
             // 2. Iterate through tokens, tracking function definitions
-            
+            var function = new FunctionAst();
+            foreach (var token in tokens)
+            {
+                switch (token.Type)
+                {
+                    case TokenType.Token:
+                        if (function.ReturnType == null)
+                            function.ReturnType = token.Value;
+                        else if (function.Name == null)
+                            function.Name = token.Value;
+                        break;
+                    case TokenType.OpenParen:
+                        break;
+                    case TokenType.CloseParen:
+                        break;
+                    case TokenType.OpenBrace:
+                        break;
+                    case TokenType.CloseBrace:
+                        break;
+                    case TokenType.Not:
+                        break;
+                    case TokenType.And:
+                        break;
+                    case TokenType.Or:
+                        break;
+                    case TokenType.Add:
+                        break;
+                    case TokenType.Minus:
+                        break;
+                    case TokenType.Multiply:
+                        break;
+                    case TokenType.Divide:
+                        break;
+                    case TokenType.Equals:
+                        break;
+                    case TokenType.Colon:
+                        break;
+                    case TokenType.SemiColon:
+                        break;
+                    case TokenType.Quote:
+                        break;
+                    case TokenType.LessThan:
+                        break;
+                    case TokenType.GreaterThan:
+                        break;
+                    case TokenType.Comma:
+                        break;
+                    case TokenType.Period:
+                        break;
+                }
+            }
         }
     }
 }
