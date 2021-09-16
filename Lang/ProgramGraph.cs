76ec24d5657e927b44b0bab2001036628c2062c2
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:31:09 -0600

    Defining all types in the assembly
    [git-p4: depot-paths = "//lang/main/": change = 275]

--- Lang/ProgramGraph.cs
+++ Lang/ProgramGraph.cs
@@ -14,9 +14,6 @@ namespace Lang
     public class Data
     {
         public List<DeclarationAst> Variables { get; } = new();
-        public Dictionary<string, IAst> Types { get; } = new();
-        public Dictionary<string, FunctionAst> Functions { get; } = new();
-        public FunctionAst Start { get; set; }
-        public List<CompilerDirectiveAst> Directives { get; } = new(); // TODO Get rid of
+        public Dictionary<string, IAst> Types { get; set; }
     }
 }
