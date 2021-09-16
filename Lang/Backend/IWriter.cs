8337a2cdc2770137d26e5072c62b28aa4c67e9d7
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:30:10 -0600

    Stubbing out LLVMWriter
    [git-p4: depot-paths = "//lang/main/": change = 96]

--- Lang/Backend/IWriter.cs
+++ Lang/Backend/IWriter.cs
@@ -3,9 +3,9 @@
     public interface IWriter
     {
         /// <summary>
-        /// Writes the program graph to a translated source file
+        /// Writes the program graph to a file
         /// </summary>
-        /// <returns>The path to the translated source file</returns>
+        /// <returns>The path to the file</returns>
         /// <param name="programGraph">Graph of the program</param>
         /// <param name="projectName">The name of the project</param>
         /// <param name="projectPath">The path to the project</param>
