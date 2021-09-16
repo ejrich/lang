8f3118d367436e5eb8f5142459d9f3a49d77e259
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:29:49 -0600

    Adding fields to ProgramGraph and hooking up the graph builder
    [git-p4: depot-paths = "//lang/main/": change = 28]

--- Lang/ProgramGraph.cs
+++ Lang/ProgramGraph.cs
@@ -1,7 +1,43 @@
-﻿namespace Lang
+﻿using System.Collections.Generic;
+
+namespace Lang
 {
     public class ProgramGraph
     {
-        
+        public Data Data { get; set; }
+        public List<Namespace> Namespaces { get; set; }
+        public List<Function> Functions { get; set; }
+        public Function Main { get; set; }
+    }
+    
+    public class Namespace
+    {
+        public string Name { get; set; }
+        public Data Data { get; set; }
+        public List<Function> Functions { get; set; }
+    }
+
+    public class Data
+    {
+        public List<Variable> Variables { get; set; }
+        public List<Struct> Structs { get; set; }
+    }
+
+    public class Struct
+    {
+        public string Name { get; set; }
+        public List<Variable> Fields { get; set; }
+    }
+
+    public class Variable
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Function
+    {
+        public List<Variable> Arguments { get; set; }
+        //public List<IAst> Body { get; set; }
     }
 }
---
+++ Lang/Translation/TranslationError.cs
@@ -0,0 +1,9 @@
+﻿namespace Lang.Translation
+{
+    public class TranslationError
+    {
+        public string Error { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
+    }
+}
