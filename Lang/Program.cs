bc739cc798523efd930d9c92649dba3bcfafce07
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:30:32 -0600

    Removing C backend
    [git-p4: depot-paths = "//lang/main/": change = 167]

--- Lang/Program.cs
+++ Lang/Program.cs
@@ -1,6 +1,5 @@
 using Lang;
 using Lang.Backend;
-using Lang.Backend.C;
 using Lang.Backend.LLVM;
 using Lang.Parsing;
 using Lang.Project;
@@ -13,11 +12,6 @@ serviceCollection.AddTransient<ILexer, Lexer>();
 serviceCollection.AddTransient<IParser, Parser>();
 serviceCollection.AddTransient<IProjectInterpreter, ProjectInterpreter>();
 serviceCollection.AddTransient<IProgramGraphBuilder, ProgramGraphBuilder>();
-// // C Backend
-// serviceCollection.AddTransient<IWriter, CWriter>();
-// serviceCollection.AddTransient<IBuilder, CBuilder>();
-// serviceCollection.AddTransient<ILinker, CLinker>();
-// serviceCollection.AddTransient<IBackend, CBackend>();
 // LLVM Backend
 serviceCollection.AddTransient<IWriter, LLVMWriter>();
 serviceCollection.AddTransient<ILinker, LLVMLinker>();
