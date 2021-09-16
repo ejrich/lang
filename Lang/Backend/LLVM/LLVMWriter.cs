5d815f0bd185990d7db7039a132c6b7e20efa22b
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:30:10 -0600

    Setting up functions
    [git-p4: depot-paths = "//lang/main/": change = 99]

--- Lang/Backend/LLVM/LLVMWriter.cs
+++ Lang/Backend/LLVM/LLVMWriter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Lang.Parsing;
@@ -29,16 +29,22 @@ namespace Lang.Backend.LLVM
             // 3. Write Data section
             WriteData(programGraph.Data);
 
-            // 4. Write Functions
+            // 4. Write Function definitions
+            foreach (var function in programGraph.Functions)
+            {
+                WriteFunctionDefinition(function.Name, function.Arguments, function.ReturnType);
+            }
+
+            // 5. Write Function bodies
             foreach (var function in programGraph.Functions)
             {
                 WriteFunction(function);
             }
 
-            // 5. Write Main function
+            // 6. Write Main function
             WriteMainFunction(programGraph.Main);
 
-            // 6. Compile to object file
+            // 7. Compile to object file
             #if DEBUG
             _module.TryPrintToFile(Path.Combine(objectPath, $"{projectName}.ll"), out _);
             #endif
@@ -61,22 +67,52 @@ namespace Lang.Backend.LLVM
             // 2. Declare variables
         }
 
-        private void WriteFunction(FunctionAst function)
+        private LLVMValueRef WriteFunctionDefinition(string name, List<Argument> arguments, TypeDefinition returnType)
         {
-            // TODO Implement me
-            // 1. Declare function definition
+            var argumentTypes = arguments.Select(_ => LLVMTypeRef.Double).ToArray();
+            var function = _module.AddFunction(name, LLVMTypeRef.CreateFunction(ConvertTypeDefinition(returnType), argumentTypes));
+            function.Linkage = LLVMLinkage.LLVMExternalLinkage;
+
+            for (var i = 0; i < arguments.Count; i++)
+            {
+                var argument = function.GetParam((uint) i);
+                argument.Name = arguments[i].Name;
+            }
+
+            return function;
+        }
+
+        private void WriteFunction(FunctionAst functionAst)
+        {
+            // 1. Get function definition
+            var function = _module.GetNamedFunction(functionAst.Name);
+            _builder.PositionAtEnd(function.AppendBasicBlock("entry"));
+
             // 2. Loop through function body
-            // 2a. Recursively write out lines
+            foreach (var ast in functionAst.Children)
+            {
+                // 2a. Recursively write out lines
+                WriteFunctionLine(ast);
+            }
+
+            // 3. Verify the function
+            function.VerifyFunction(LLVMVerifierFailureAction.LLVMPrintMessageAction);
         }
 
         private void WriteMainFunction(FunctionAst main)
         {
-            // TODO Implement me
-            var arguments = Array.Empty<LLVMTypeRef>();
-            var function = _module.AddFunction("main", LLVMTypeRef.CreateFunction(LLVMTypeRef.Int32, arguments));
-            function.Linkage = LLVMLinkage.LLVMExternalLinkage;
+            // 1. Define main function
+            var function = WriteFunctionDefinition("main", main.Arguments, main.ReturnType);
             _builder.PositionAtEnd(function.AppendBasicBlock("entry"));
-            _builder.BuildRet(LLVMValueRef.CreateConstInt(LLVMTypeRef.Int32, 0));
+
+            // 2. Loop through function body
+            foreach (var ast in main.Children)
+            {
+                // 2a. Recursively write out lines
+                WriteFunctionLine(ast);
+            }
+
+            // 3. Verify the function
             function.VerifyFunction(LLVMVerifierFailureAction.LLVMPrintMessageAction);
         }
 
@@ -95,6 +131,109 @@ namespace Lang.Backend.LLVM
             _module.DataLayout = targetMachine.CreateTargetDataLayout();
 
             targetMachine.EmitToFile(_module, objectFile, LLVMCodeGenFileType.LLVMObjectFile);
+        }
+
+        private void WriteFunctionLine(IAst ast)
+        {
+            switch (ast)
+            {
+                case ReturnAst returnAst:
+                    WriteReturnStatement(returnAst);
+                    break;
+                case DeclarationAst declaration:
+                    WriteDeclaration(declaration);
+                    break;
+                case AssignmentAst assignment:
+                    WriteAssignment(assignment);
+                    break;
+                case ScopeAst scope:
+                    WriteScope(scope.Children);
+                    break;
+                case ConditionalAst conditional:
+                    WriteConditional(conditional);
+                    break;
+                case WhileAst whileAst:
+                    WriteWhile(whileAst);
+                    break;
+                case EachAst each:
+                    WriteEach(each);
+                    break;
+                default:
+                    WriteExpression(ast);
+                    break;
+            }
+        }
+
+        private void WriteReturnStatement(ReturnAst returnAst)
+        {
+            // 1. Determine to write constant, variable, or evaluate an expression
+            switch (returnAst.Value)
+            {
+                case ConstantAst constant:
+                    var type = ConvertTypeDefinition(constant.Type);
+                    switch (type.Kind)
+                    {
+                        case LLVMTypeKind.LLVMIntegerTypeKind:
+                            _builder.BuildRet(LLVMValueRef.CreateConstInt(type, ulong.Parse(constant.Value), true));
+                            break;
+                    }
+                    return;
+                case VariableAst variable:
+                    break;
+            }
+
+            // 2. Write expression as return value
+            _builder.BuildRet(LLVMValueRef.CreateConstInt(LLVMTypeRef.Int32, 0));
+        }
+
+        private void WriteDeclaration(DeclarationAst declaration)
+        {
+            // TODO Implement me
+            // _builder.BuildAlloca();
+        }
+        
+        private void WriteAssignment(AssignmentAst assignment)
+        {
+            // TODO Implement me
+        }
+
+        private void WriteScope(List<IAst> scopeChildren)
+        {
+            // TODO Implement me
+        }
+
+        private void WriteConditional(ConditionalAst conditional)
+        {
+            // TODO Implement me
+        }
+
+        private void WriteWhile(WhileAst whileAst)
+        {
+            // TODO Implement me
+        }
+
+        private void WriteEach(EachAst each)
+        {
+            // TODO Implement me
+        }
+
+        private void WriteExpression(IAst ast)
+        {
+            // TODO Implement me
+        }
+
+        private static LLVMTypeRef ConvertTypeDefinition(TypeDefinition typeDef)
+        {
+            switch (typeDef.Name)
+            {
+                case "int":
+                    return LLVMTypeRef.Int32;
+                case "float":
+                    return LLVMTypeRef.Float;
+                // TODO Add more type inference
+                default:
+                    return LLVMTypeRef.Double;
+            }
         }
     }
 }
