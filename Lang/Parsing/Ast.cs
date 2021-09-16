378d6e69acf41d5a14606534b1b4a742697dc0b3
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:29:44 -0600

    Starting work on the parser
    [git-p4: depot-paths = "//lang/main/": change = 12]

---
+++ Lang/Parsing/Ast.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Lang.Parsing
+{
+    public class Variable
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class FunctionAst
+    {
+        public string Name { get; set; }
+        public string ReturnType { get; set; }
+        public List<Variable> Arguments { get; } = new();
+        public List<Token> Body { get; } = new();
+    }
+}
