95ab28984175225a2621a7a480269a3b668cf5e8
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:30:43 -0600

    Linking dependencies from the project file
    [git-p4: depot-paths = "//lang/main/": change = 199]

--- Lang/Backend/Assembly/AssemblyLinker.cs
+++ Lang/Backend/Assembly/AssemblyLinker.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+
 namespace Lang.Backend.Assembly
 {
     public class AssemblyLinker : ILinker
     {
-        public void Link(string objectFile, string projectPath)
+        public void Link(string objectFile, string projectPath, List<string> dependencies)
         {
             throw new System.NotImplementedException();
         }
--- Lang/Project/ProjectFile.cs
+++ Lang/Project/ProjectFile.cs
@@ -13,7 +13,7 @@ namespace Lang.Project
     public class ProjectFile
     {
         public string Name { get; set; }
-        public List<string> Dependencies { get; set; } = new();
-        public List<string> Packages { get; set; } = new();
+        public List<string> Dependencies { get; } = new();
+        public List<string> Packages { get; } = new();
     }
 }
