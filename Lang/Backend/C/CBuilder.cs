bb83337fda28cc021fbbbfd0ca1e144bc7bfef2c
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:29:52 -0600

    Compiling cpp file to object file
    [git-p4: depot-paths = "//lang/main/": change = 38]

--- Lang/Backend/C/CBuilder.cs
+++ Lang/Backend/C/CBuilder.cs
@@ -1,11 +1,25 @@
-﻿namespace Lang.Backend.C
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Lang.Backend.C
 {
     public class CBuilder : IBuilder
     {
         public string BuildTranslatedFile(string filePath)
         {
-            // TODO Implement me
-            return string.Empty;
+            var objectFile = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".o");
+            var buildProcess = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "g++",
+                    Arguments = $"-c {filePath} -o {objectFile}"
+                }
+            };
+            buildProcess.Start();
+            buildProcess.WaitForExit();
+
+            return objectFile;
         }
     }
 }
