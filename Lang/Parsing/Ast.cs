daa06ceb31642cd6bcad0a50a651a8bd576a074d
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:30:19 -0600

    Passing type definitions through llvm writer
    [git-p4: depot-paths = "//lang/main/": change = 126]

--- Lang/Parsing/Ast.cs
+++ Lang/Parsing/Ast.cs
@@ -56,6 +56,7 @@ namespace Lang.Parsing
         public int Line { get; init; }
         public int Column { get; init; }
         public string Name { get; set; }
+        public string StructName { get; set; }
         public StructFieldRefAst Value { get; set; }
         public int ValueIndex { get; set; }
         public List<IAst> Children => null;
