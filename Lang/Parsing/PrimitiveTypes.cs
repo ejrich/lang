3ea63cd349230458a02cc87936df79af3ae76352
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:30:17 -0600

    Implementing u64, s64, u32, s32, u16, s16, u8, s8, and float64 primitive type parsing, still have to implement unsigned instructions
    [git-p4: depot-paths = "//lang/main/": change = 120]

---
+++ Lang/Parsing/PrimitiveTypes.cs
@@ -0,0 +1,19 @@
+﻿namespace Lang.Parsing
+{
+    public interface IPrimitive
+    {
+        ushort Bytes { get; init; }
+    }
+
+    public class IntegerType : IPrimitive
+    {
+        public ushort Bytes { get; init; }
+        public bool Signed { get; init; }
+    }
+
+    public class FloatType : IPrimitive
+    {
+        public ushort Bytes { get; init; }
+        public bool Double => Bytes == 64;
+    }
+}
