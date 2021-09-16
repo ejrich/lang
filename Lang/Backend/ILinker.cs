8d24482893d89bf199544be9e9c784b5e037a690
Evan Richardson <ejrich44@gmail.com>
2021-09-16 10:29:52 -0600

    Hooking up the linker
    [git-p4: depot-paths = "//lang/main/": change = 39]

--- Lang/Backend/ILinker.cs
+++ Lang/Backend/ILinker.cs
@@ -5,8 +5,8 @@
         /// <summary>
         /// Links the object file and creates an executable file
         /// </summary>
-        /// <param name="objectPath">Path to the object file</param>
-        /// <param name="executableName">Name of the executable file</param>
-        void Link(string objectPath, string executableName);
+        /// <param name="objectFile">Path to the object file</param>
+        /// <param name="projectPath">Name of the executable file</param>
+        void Link(string objectFile, string projectPath);
     }
 }
