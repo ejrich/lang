1feac6ee28f434ca4eacda33baa4006ea27c2586
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:29:53 -0600

    Setting up writer
    [git-p4: depot-paths = "//lang/main/": change = 40]

--- Lang/Backend/C/CWriter.cs
+++ Lang/Backend/C/CWriter.cs
@@ -18,17 +18,49 @@ namespace Lang.Backend.C
             var fileContents = new StringBuilder();
 
             // 2. Write Data section
+            AppendData(fileContents, programGraph.Data);
+
             // 3. Write Namespaces
+            foreach (var ns in programGraph.Namespaces)
+            {
+                AppendNamespace(fileContents, ns);
+            }
+
             // 4. Write Functions
+            foreach (var function in programGraph.Functions)
+            {
+                AppendFunction(fileContents, function);
+            }
+
             // 5. Write Main function
-            fileContents.AppendLine("int main() {");
-            fileContents.AppendLine("\treturn 0;");
-            fileContents.AppendLine("}");
+            AppendMainFunction(fileContents, programGraph.Main);
 
             // 6. Write to file
             File.WriteAllText(file, fileContents.ToString());
 
             return file;
+        }
+
+        private static void AppendData(StringBuilder fileContents, Data data)
+        {
+            // TODO Write out data
+        }
+
+        private static void AppendNamespace(StringBuilder fileContents, Namespace ns)
+        {
+            // TODO Write out data and functions in namespace
+        }
+
+        private static void AppendFunction(StringBuilder fileContents, Function function)
+        {
+            // TODO Write out functions
+        }
+
+        private static void AppendMainFunction(StringBuilder fileContents, Function main)
+        {
+            fileContents.AppendLine("int main() {");
+            fileContents.AppendLine("\treturn 0;");
+            fileContents.AppendLine("}");
         }
     }
 }
