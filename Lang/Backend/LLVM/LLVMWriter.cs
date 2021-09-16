40aa5abac715572641c18dbfaf86e7270486a768
evan <evan@_____CLIENT_UNSET_____>
2021-09-16 10:30:09 -0600

    Setting up LLVM writer and linker
    [git-p4: depot-paths = "//lang/main/": change = 95]

--- Lang/Backend/LLVM/LLVMWriter.cs
+++ Lang/Backend/LLVM/LLVMWriter.cs
@@ -1,10 +1,44 @@
-﻿namespace Lang.Backend.LLVM
+﻿using System.IO;
+using System.Text;
+using LLVMSharp.Interop;
+
+namespace Lang.Backend.LLVM
 {
     public class LLVMWriter : IWriter
     {
+        private const string ObjectDirectory = "obj";
+
+        private LLVMModuleRef _module;
+        private LLVMBuilderRef _builder;
+
         public string WriteFile(ProgramGraph programGraph, string projectName, string projectPath)
         {
-            throw new System.NotImplementedException();
+            // 1. Initialize the LLVM module and builder
+            InitLLVM(projectName);
+
+            // 2. Verify obj directory exists
+            var objectPath = Path.Combine(projectPath, ObjectDirectory);
+            if (!Directory.Exists(objectPath))
+                Directory.CreateDirectory(objectPath);
+
+            // 3. Write Data section
+
+            // 4. Write Functions
+            foreach (var function in programGraph.Functions)
+            {
+            }
+
+            // 5. Write Main function
+
+            // 6. Write to file
+
+            return string.Empty; // TODO put object file path here
+        }
+
+        private void InitLLVM(string projectName)
+        {
+            _module = LLVMModuleRef.CreateWithName(projectName);
+            _builder = LLVMBuilderRef.Create(_module.Context);
         }
     }
 }
