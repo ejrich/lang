94813e467a7235a096e3ab404d6e42b2ee488c70
Evan Richardson <ejrich44@gmail.com>
2021-10-08 06:53:32 -0600

    Removing project interpreter
    [git-p4: depot-paths = "//lang/main/": change = 833]

--- Lang/Main.cs
+++ Lang/Main.cs
@@ -6,7 +6,6 @@ var serviceCollection = new ServiceCollection();
 serviceCollection.AddTransient<ICompiler, Compiler>();
 serviceCollection.AddTransient<ILexer, Lexer>();
 serviceCollection.AddTransient<IParser, Parser>();
-serviceCollection.AddTransient<IProjectInterpreter, ProjectInterpreter>();
 serviceCollection.AddTransient<ITypeChecker, TypeChecker>();
 serviceCollection.AddTransient<IPolymorpher, Polymorpher>();
 serviceCollection.AddTransient<IProgramRunner, ProgramRunner>();
