2049db979e019ea17c40394c0fc13a425c388314
Evan Richardson <ejrich44@gmail.com>
2021-11-18 14:54:49 -0600

    Remove IsLetter
    [git-p4: depot-paths = "//lang/main/": change = 916]

--- ol/Lexer.cs
+++ ol/Lexer.cs
@@ -281,7 +281,6 @@ namespace ol
         private static TokenType GetTokenType(char character)
         {
             if (char.IsDigit(character)) return TokenType.Number;
-            if (char.IsLetter(character)) return TokenType.Identifier;
 
             return character switch
             {
